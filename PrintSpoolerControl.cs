a7d104fc87f07072b7769f0cc3157ce21260082b
Sami Hammal <sami.hammal@emtech.gr>
2013-05-31 09:46:57 +0300

    Initial Commit

---
+++ PrintSpoolerControl.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.ServiceProcess;
+
+namespace PrintSpoolerControl
+{
+    class PrintSpoolerControl : System.ServiceProcess.ServiceBase
+    {
+
+        static private string folderPath = @"C:\Users\Sam\Desktop\PrintSpoolerControlLog.txt";
+        static System.Timers.Timer timer;
+
+        private static void TimerEventProcessor(Object sender, System.Timers.ElapsedEventArgs e)
+        {
+
+            string serviceName = "Spooler";
+
+            ServiceController service = new ServiceController(serviceName);
+
+            ServiceControllerStatus status = service.Status;
+
+            if (!status.Equals(ServiceControllerStatus.Running))
+            {
+
+                try
+                {
+
+                    string msg = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+                    msg += " Spooler service had stoped";
+
+                    System.IO.StreamWriter file = new System.IO.StreamWriter(folderPath, true);
+                    file.WriteLine(msg);
+
+                    file.Close();
+
+                    System.IO.DirectoryInfo downloadedMessageInfo = new DirectoryInfo(@"C:\Windows\System32\spool\PRINTERS\");
+
+                    foreach (FileInfo prfile in downloadedMessageInfo.GetFiles())
+                    {
+                        prfile.Delete();
+                    }
+
+                    service.Start();
+                }
+                catch
+                {
+                }
+
+            }
+
+        }
+
+        // The main entry point for the process
+        static void Main()
+        {
+
+            System.ServiceProcess.ServiceBase[] ServicesToRun;
+            ServicesToRun =
+              new System.ServiceProcess.ServiceBase[] { new PrintSpoolerControl() };
+            System.ServiceProcess.ServiceBase.Run(ServicesToRun);
+        }
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ServiceName = "PrintSpoolerControl";
+        }
+        
+        /// <summary>
+        /// Set things in motion so your service can do its work.
+        /// </summary>
+        protected override void OnStart(string[] args)
+        {
+            
+            timer = new System.Timers.Timer();
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(TimerEventProcessor);
+            timer.Interval = 60000;
+            timer.Start();
+
+        }
+        /// <summary>
+        /// Stop this service.
+        /// </summary>
+        protected override void OnStop()    
+        {
+            timer.Stop();
+        }
+
+    }
+}
---
+++ ServiceInstaller.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrintSpoolerControl
+{
+    /// <summary>
+    ///     Summary description for ProjectInstaller.
+    /// </summary>
+    [System.ComponentModel.RunInstaller(true)]
+    public class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        /// <summary>
+        ///    Required designer variable.
+        /// </summary>
+        //private System.ComponentModel.Container components;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller;
+        private System.ServiceProcess.ServiceProcessInstaller
+                serviceProcessInstaller;
+
+        public ProjectInstaller()
+        {
+            // This call is required by the Designer.
+            InitializeComponent();
+        }
+
+        /// <summary>
+        ///    Required method for Designer support - do not modify
+        ///    the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceInstaller = new System.ServiceProcess.ServiceInstaller();
+            this.serviceProcessInstaller =
+              new System.ServiceProcess.ServiceProcessInstaller();
+            // 
+            // serviceInstaller
+            // 
+            this.serviceInstaller.Description = "Print Spooler Control Service";
+            this.serviceInstaller.DisplayName = "Print Spooler Control";
+            this.serviceInstaller.ServiceName = "PrintSpoolerControl";
+            // 
+            // serviceProcessInstaller
+            // 
+            this.serviceProcessInstaller.Account =
+              System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller.Password = null;
+            this.serviceProcessInstaller.Username = null;
+            // 
+            // ServiceInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller,
+            this.serviceInstaller});
+
+        }
+    }
+}
